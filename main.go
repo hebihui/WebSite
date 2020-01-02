1277d46ed26ffe7f6f8692a1d3c39a1a8958ee98
hebihui <hebihuei@gmail.com>
2020-01-02 22:42:07 +0800

    minimum site

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handleDefault(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path == "/" {
+		http.ServeFile(w, req, "www/index.html")
+		return
+	}
+	req.URL.RawQuery = "" // skip "?params"
+	wwwServer.ServeHTTP(w, req)
+}
+
+var (
+	wwwServer = http.FileServer(http.Dir("www"))
+)
+
+func main() {
+	http.HandleFunc("/", handleDefault)
+	http.ListenAndServe(":8888", nil)
+}
